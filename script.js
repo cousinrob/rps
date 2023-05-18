21a9d2096bec08021c4278fd49ac9fb6f8016057
Maris <131636690+cousinrob@users.noreply.github.com>
2023-05-18 13:30:03 -0400

    Polish computer choice message Add game round mechanic and tie result

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function getComputerChoice() {
-        let rps = ['Rock','Paper','Scissors'] ;
+        let rps = ['rock','paper','scissors'] ;
         const choice = Math.floor(Math.random() * rps.length);
-        return("The computer chose " + rps[choice]);
+        return(rps[choice]);
     }
+
+const computerChoice = getComputerChoice();
+
+const playerChoice = "rock";
+
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice===playerChoice.toLowerCase()) {
+        return "It's a tie!";
+    }
+}
