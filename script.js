cc4f557553e8f8dddc253642b2a7d2f9c8cef419
Maris <131636690+cousinrob@users.noreply.github.com>
2023-05-18 13:02:38 -0400

    Add initial computer choice selector

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+    function getComputerChoice {
+        let rps = ['Rock','Paper','Scissors'] ;
+        const choice = Math.floor(Math.random() * rps.length);
+        console.log(choice);
+    }
+</script>
