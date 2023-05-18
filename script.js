d1ff3c11b34601a964a1d2b97bf80fcbbba5976a
Maris <131636690+cousinrob@users.noreply.github.com>
2023-05-18 14:28:29 -0400

    Polish tie message Add score variable Add game function

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ function getComputerChoice() {
         return(rps[choice]);
     }
 
-const computerChoice = getComputerChoice();
+    const computerChoice = getComputerChoice();
 
-const playerChoice = prompt('Choose rock, paper, or scissors');
+    const playerChoice = prompt('Choose rock, paper, or scissors');
+
+    let score = 0;
 
 function playRound(computerChoice, playerChoice) {
     if (computerChoice===playerChoice.toLowerCase()) {
-        return "It's a tie!";
+        return "It's a tie! your score is " + score + " out of 5";
     }
     else {
         return "The computer chose " + computerChoice;
     }
 }
 
-console.log(playRound(computerChoice, playerChoice));
+function game() {
+    console.log(playRound(computerChoice, playerChoice));
+    console.log(playRound(computerChoice, playerChoice));
+    console.log(playRound(computerChoice, playerChoice));
+    console.log(playRound(computerChoice, playerChoice));
+    console.log(playRound(computerChoice, playerChoice));
+}
+
+console.log(game())
