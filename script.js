f694ff737e77a76af7461eaa44b622778c525f20
Maris <131636690+cousinrob@users.noreply.github.com>
2023-05-18 13:06:05 -0400

    Add parentheses in line 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice {
+function getComputerChoice() {
         let rps = ['Rock','Paper','Scissors'] ;
         const choice = Math.floor(Math.random() * rps.length);
         console.log(choice);
