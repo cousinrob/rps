4e2b9005c15179e3edae005131f2de0c355a0f04
Maris <131636690+cousinrob@users.noreply.github.com>
2023-09-25 16:07:27 -0400

    Add score mechanic and visual

--- script.js
+++ script.js
@@ -8,44 +8,46 @@ function getComputerChoice() {
     let computerChoice = getComputerChoice();
 
 
-
+    let computerScore = 0;
     let score = 0;
     const result = document.querySelector('#result');
     const scorecard = document.querySelector('#score');
+    const scoreFinal = document.querySelector("#finalscore");
 
 function playRound(computerChoice, playerChoice) {
 
     if (computerChoice===playerChoice.toLowerCase()) {
         result.textContent = "It's a tie!"
-        scorecard.textContent= "Your score is " + score + " out of 5.";
+        scorecard.textContent= "Your score is " + score + " out of 5. The computer's score is " + computerScore + " out of 5.";
     }
     else if (computerChoice==='rock' && playerChoice.toLowerCase()==='paper'){
         result.textContent = "The computer chose " + computerChoice + ". You won!";
-        scorecard.textContent = "Your score is " + ++score + " out of 5.";
+        scorecard.textContent = "Your score is " + ++score + " out of 5. The computer's score is " + computerScore + " out of 5.";
     }
     else if (computerChoice==='paper' && playerChoice.toLowerCase()==='rock'){
         result.textContent = "The computer chose " + computerChoice + ". You lost!";
-        scorecard.textContent = "Your score is " + score + " out of 5.";
+        scorecard.textContent = "Your score is " + score + " out of 5. The computer's score is " + ++computerScore + " out of 5.";
     }
     else if (computerChoice==='rock' && playerChoice.toLowerCase()==='scissors'){
         result.textContent = "The computer chose " + computerChoice + ". You lost!";
-        scorecard.textContent = "Your score is " + score + " out of 5.";
+        scorecard.textContent = "Your score is " + score + " out of 5. The computer's score is " + ++computerScore + " out of 5.";
     }
     else if (computerChoice==='scissors' && playerChoice.toLowerCase()==='rock'){
         result.textContent = "The computer chose " + computerChoice + ". You won!";
-        scorecard.textContent="Your score is " + ++score + " out of 5.";
+        scorecard.textContent="Your score is " + ++score + " out of 5. The computer's score is " + computerScore + " out of 5.";
     }
     else if (computerChoice==='paper' && playerChoice.toLowerCase()==='scissors'){
         result.textContent = "The computer chose " + computerChoice + ". You won!";
-        scorecard.textContent= "Your score is " + ++score + " out of 5.";
+        scorecard.textContent= "Your score is " + ++score + " out of 5. The computer's score is " + computerScore + " out of 5.";
     }
     else if (computerChoice==='scissors' && playerChoice.toLowerCase()==='paper'){
         result.textContent = "The computer chose " + computerChoice + ". You lost!";
-        scorecard.textContent = "Your score is " + score + " out of 5.";
+        scorecard.textContent = "Your score is " + score + " out of 5. The computer's score is " + ++computerScore + " out of 5.";
     }
     else {
         result.textContent = "Your response was invalid";
-    }
+    };
+    finalScore();
 }
 
 const rock = document.querySelector('#rock');
@@ -68,22 +70,22 @@ scissors.addEventListener("click", function(e) {
 
 
 function finalScore() {
-    if (score===0) {
-        return "Your score is 0 out of 5! Ouch...a definite loss.";
+    if (computerScore===5 && score===0) {
+        scoreFinal.textContent= "Your score is 0 out of 5! Ouch...a definite loss.";
     }
-    else if (score===1) {
-        return "Your score is 1 out of 5! Well...at least you got something.";
+    else if (computerScore===5 && score===1) {
+        scoreFinal.textContent= "Your score is 1 out of 5! Well...at least you got something.";
     }
-    else if (score===2) {
-        return "Your score is 2 out of 5! I guess you're not a total loser?";
+    else if (computerScore===5 && score===2) {
+        scoreFinal.textContent= "Your score is 2 out of 5! I guess you're not a total loser?";
     }
-    else if (score===3) {
-        return "Your score is 3 out of 5! So close!";
+    else if (computerScore===5 && score===3) {
+        scoreFinal.textContent= "Your score is 3 out of 5! So close!";
     }
-    else if (score===4) {
-        return "Your score is 4 out of 5! Good show!";
+    else if (computerScore===5 && score===4) {
+        scoreFinal.textContent= "Your score is 4 out of 5! Good show!";
     }
     else if (score===5) {
-        return "Your score is 5 out of 5! You're the champion!";
+        scoreFinal.textContent= "Your score is 5 out of 5! You're the champion!";
     }
 }
